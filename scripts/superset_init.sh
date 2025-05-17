eb7c6f66c3b1990e66ff7b66143f953ce75b0d56
Pedro Melo <pedromelo120@gmail.com>
2025-05-17 12:46:45 +0100

    initial commit

---
+++ scripts/superset_init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Inicializa o banco de dados do Superset (migrations)
+superset db upgrade
+
+# Cria o usuário admin (altera user/password/email se quiseres)
+superset fab create-admin \
+    --username admin \
+    --firstname Admin \
+    --lastname User \
+    --email admin@example.com \
+    --password admin
+
+# Carrega configurações iniciais (roles, permissões)
+superset init
+
+# (Opcional) Carrega exemplos de dashboards e dados
+# superset load-examples
+
+echo "Superset inicializado com sucesso!"
